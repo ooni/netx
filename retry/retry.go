f85f6fd6b410d49c1ad7b3c6b4e796c121124e58
Simone Basso <bassosimone@gmail.com>
2019-09-15 18:54:46 +0200

    Write a standalone implementation of retry

---
+++ retry/retry.go
@@ -0,0 +1,40 @@
+// Package retry contains code to retry operations.
+package retry
+
+import (
+	"context"
+	"errors"
+	"math/rand"
+	"time"
+)
+
+// TODO(bassosimone): we need to calibrate these parameters.
+const (
+	initialMean = 0.5
+	finalMean   = 8.0
+	meanFactor  = 2.0
+	stdevFactor = 0.05
+)
+
+// Retry retries op until it succeds, context expires, or we've
+// attempted to retry the operation for too much time.
+func Retry(ctx context.Context, op func() error) error {
+	rng := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for mean := initialMean; mean <= finalMean; mean *= meanFactor {
+		if err := op(); err == nil {
+			return nil
+		}
+		stdev := stdevFactor * mean
+		seconds := rng.NormFloat64()*stdev + mean
+		sleepTime := time.Duration(seconds * float64(time.Second))
+		timer := time.NewTimer(sleepTime)
+		select {
+		case <-ctx.Done():
+			timer.Stop()
+			return ctx.Err()
+		case <-timer.C:
+			// FALLTHROUGH
+		}
+	}
+	return errors.New("retry.Retry: all attempts failed")
+}
