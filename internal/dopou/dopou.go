f588cca0815647eb320e96076dc6a937fd9c0554
Simone Basso <bassosimone@gmail.com>
2019-09-19 14:56:22 +0200

    dopou: use dnsoverudp

--- internal/dopou/dopou.go
+++ internal/dopou/dopou.go
@@ -4,23 +4,21 @@ package dopou
 import (
 	"context"
 	"net"
-	"time"
 
 	"github.com/bassosimone/netx/internal/dialerapi"
+	"github.com/bassosimone/netx/internal/dnstransport/dnsoverudp"
 	"github.com/bassosimone/netx/internal/dox"
 )
 
 // Client is a DNS over plain old UDP client
 type Client struct {
-	address string
-	dialer  *dialerapi.Dialer
+	transport *dnsoverudp.Transport
 }
 
 // NewClient creates a new DoPOU client.
 func NewClient(dialer *dialerapi.Dialer, address string) (*Client, error) {
 	return &Client{
-		address: address,
-		dialer:  dialer,
+		transport: dnsoverudp.NewTransport(dialer.Beginning, dialer.Handler, address),
 	}, nil
 }
 
@@ -37,41 +35,19 @@ func (clnt *Client) NewResolver() *net.Resolver {
 
 // NewConn returns a new dopou pseudo-conn
 func (clnt *Client) NewConn() (net.Conn, error) {
-	return dox.NewConn(clnt.dialer.Beginning, clnt.dialer.Handler, func(b []byte) dox.Result {
+	beginning := clnt.transport.Dialer.Beginning
+	handler := clnt.transport.Dialer.Handler
+	return dox.NewConn(beginning, handler, func(b []byte) dox.Result {
 		return clnt.do(b)
 	}), nil
 }
 
 // RoundTrip implements the dnsx.RoundTripper interface
 func (clnt *Client) RoundTrip(query []byte) (reply []byte, err error) {
-	out := clnt.do(query)
-	reply = out.Data
-	err = out.Err
-	return
+	return clnt.transport.RoundTrip(query)
 }
 
 func (clnt *Client) do(b []byte) (out dox.Result) {
-	var conn net.Conn
-	conn, _, _, out.Err = clnt.dialer.DialContextEx(
-		context.Background(), "udp", clnt.address, true,
-	)
-	if out.Err != nil {
-		return
-	}
-	defer conn.Close()
-	out.Err = conn.SetDeadline(time.Now().Add(3 * time.Second))
-	if out.Err != nil {
-		return
-	}
-	_, out.Err = conn.Write(b)
-	if out.Err != nil {
-		return
-	}
-	out.Data = make([]byte, 1<<17)
-	var n int
-	n, out.Err = conn.Read(out.Data)
-	if out.Err == nil {
-		out.Data = out.Data[:n]
-	}
+	out.Data, out.Err = clnt.RoundTrip(b)
 	return
 }
