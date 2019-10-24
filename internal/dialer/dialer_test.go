bca0584154c897a8d73db8ce499f179ea8c64854
Simone Basso <bassosimone@gmail.com>
2019-10-24 17:53:29 +0200

    dialer: refactor DNS handling code in dnsdialer

--- internal/dialer/dialer_test.go
+++ internal/dialer/dialer_test.go
@@ -1,7 +1,6 @@
 package dialer
 
 import (
-	"context"
 	"crypto/tls"
 	"crypto/x509"
 	"testing"
@@ -42,98 +41,6 @@ func TestIntegrationInvalidAddress(t *testing.T) {
 func TestIntegrationInvalidAddressTLS(t *testing.T) {
 	dialer := NewDialer(time.Now(), handlers.NoHandler)
 	conn, err := dialer.DialTLS("tcp", "www.google.com")
-	if err == nil {
-		t.Fatal("expected an error here")
-	}
-	if conn != nil {
-		t.Fatal("expected a nil conn here")
-	}
-}
-
-func TestIntegrationDialContextExIPAddress(t *testing.T) {
-	dialer := NewDialer(time.Now(), handlers.NoHandler)
-	conn, onlyhost, onlyport, _, err := dialer.DialContextEx(
-		context.Background(), "tcp", "8.8.8.8:443", true,
-	)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if onlyhost != "8.8.8.8" {
-		t.Fatal("unexpected onlyhost value")
-	}
-	if onlyport != "443" {
-		t.Fatal("unexpected onlyport value")
-	}
-	if conn == nil {
-		t.Fatal("expected a non-nil conn here")
-	}
-	conn.Close()
-}
-
-func TestIntegrationUnexpectedDomain(t *testing.T) {
-	dialer := NewDialer(time.Now(), handlers.NoHandler)
-	conn, onlyhost, onlyport, _, err := dialer.DialContextEx(
-		context.Background(), "tcp", "www.google.com:443", true,
-	)
-	if err == nil {
-		t.Fatal("expected an error here")
-	}
-	if onlyhost != "www.google.com" {
-		t.Fatal("unexpected onlyhost value")
-	}
-	if onlyport != "443" {
-		t.Fatal("unexpected onlyport value")
-	}
-	if conn != nil {
-		t.Fatal("expected a nil conn here")
-	}
-}
-
-func TestIntegrationLookupFailure(t *testing.T) {
-	dialer := NewDialer(time.Now(), handlers.NoHandler)
-	conn, onlyhost, onlyport, _, err := dialer.DialContextEx(
-		context.Background(), "tcp", "antani.ooni.io:443", false,
-	)
-	if err == nil {
-		t.Fatal("expected an error here")
-	}
-	if onlyhost != "antani.ooni.io" {
-		t.Fatal("unexpected onlyhost value")
-	}
-	if onlyport != "443" {
-		t.Fatal("unexpected onlyport value")
-	}
-	if conn != nil {
-		t.Fatal("expected a nil conn here")
-	}
-}
-
-func TestIntegrationDialTCPFailure(t *testing.T) {
-	if testing.Short() {
-		t.Skip()
-	}
-	dialer := NewDialer(time.Now(), handlers.NoHandler)
-	// The port is unreachable and filtered. The timeout is here
-	// to make sure that we don't run for too much time.
-	ctx, cancel := context.WithTimeout(context.Background(), 4*time.Second)
-	defer cancel()
-	conn, err := dialer.DialContext(ctx, "tcp", "ooni.io:12345")
-	if err == nil {
-		t.Fatal("expected an error here")
-	}
-	if conn != nil {
-		t.Fatal("expected a nil conn here")
-	}
-}
-
-func TestDialDNSFailure(t *testing.T) {
-	dialer := NewDialer(time.Now(), handlers.NoHandler)
-	// The insane timeout is such that the DNS resolver fails because it
-	// times out when trying to dial for the default server. (This is
-	// a test that only makes sense on Unix.)
-	ctx, cancel := context.WithTimeout(context.Background(), 1)
-	defer cancel()
-	conn, err := dialer.DialContext(ctx, "tcp", "ooni.io:80")
 	if err == nil {
 		t.Fatal("expected an error here")
 	}
---
+++ internal/dialer/dnsdialer/dnsdialer.go
@@ -0,0 +1,96 @@
+// Package dnsdialer contains a dialer with DNS lookups.
+package dnsdialer
+
+import (
+	"context"
+	"errors"
+	"net"
+	"sync/atomic"
+	"time"
+
+	"github.com/ooni/netx/internal/dialer/dialerbase"
+	"github.com/ooni/netx/model"
+)
+
+var nextDialID, nextConnID int64
+
+// Dialer defines the dialer API. We implement the most basic form
+// of DNS, but more advanced resolutions are possible.
+type Dialer struct {
+	beginning time.Time
+	dialer    model.Dialer
+	handler   model.Handler
+	resolver  model.DNSResolver
+}
+
+// New creates a new Dialer.
+func New(
+	beginning time.Time, handler model.Handler,
+	resolver model.DNSResolver, dialer model.Dialer,
+) (d *Dialer) {
+	return &Dialer{
+		beginning: beginning,
+		dialer:    dialer,
+		handler:   handler,
+		resolver:  resolver,
+	}
+}
+
+// Dial creates a TCP or UDP connection. See net.Dial docs.
+func (d *Dialer) Dial(network, address string) (net.Conn, error) {
+	return d.DialContext(context.Background(), network, address)
+}
+
+// DialContext is like Dial but the context allows to interrupt a
+// pending connection attempt at any time.
+func (d *Dialer) DialContext(
+	ctx context.Context, network, address string,
+) (conn net.Conn, err error) {
+	onlyhost, onlyport, err := net.SplitHostPort(address)
+	if err != nil {
+		return nil, err
+	}
+	dialID := atomic.AddInt64(&nextDialID, 1)
+	connID := atomic.AddInt64(&nextConnID, 1)
+	if net.ParseIP(onlyhost) != nil {
+		dialer := dialerbase.New(
+			d.beginning, d.handler, d.dialer, dialID, connID,
+		)
+		conn, err = dialer.DialContext(ctx, network, address)
+		return
+	}
+	start := time.Now()
+	var addrs []string
+	addrs, err = d.resolver.LookupHost(ctx, onlyhost)
+	stop := time.Now()
+	d.handler.OnMeasurement(model.Measurement{
+		Resolve: &model.ResolveEvent{
+			Addresses: addrs,
+			DialID:    dialID,
+			Duration:  stop.Sub(start),
+			Error:     err,
+			Hostname:  onlyhost,
+			Time:      stop.Sub(d.beginning),
+		},
+	})
+	if err != nil {
+		return
+	}
+	for _, addr := range addrs {
+		dialer := dialerbase.New(
+			d.beginning, d.handler, d.dialer, dialID, connID,
+		)
+		target := net.JoinHostPort(addr, onlyport)
+		conn, err = dialer.DialContext(ctx, network, target)
+		if err == nil {
+			return
+		}
+		connID = atomic.AddInt64(&nextConnID, 1)
+	}
+	err = &net.OpError{
+		Op:  "dial",
+		Net: network,
+		Err: errors.New("all connect attempts failed"),
+	}
+	return
+}
