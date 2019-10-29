c4a8fc9645b8718abfe03be9bda2fc6de2053c8d
Simone Basso <bassosimone@gmail.com>
2019-10-29 11:44:46 +0100

    much better tests for porcelain

--- x/porcelain/porcelain_test.go
+++ x/porcelain/porcelain_test.go
@@ -1,11 +1,33 @@
 package porcelain
 
-import "testing"
+import (
+	"bytes"
+	"io/ioutil"
+	"strings"
+	"testing"
+)
 
 func TestIntegration(t *testing.T) {
-	req, err := NewHTTPRequest("GET", "http://www.x.org", nil)
+	body := strings.NewReader("antani")
+	req, err := NewHTTPRequest("POST", "http://www.x.org", body)
 	if err != nil {
 		t.Fatal(err)
+	}
+	if req.Method != "POST" {
+		t.Fatal("unexpected method")
+	}
+	if req.URL.Scheme != "http" {
+		t.Fatal("unexpected scheme")
+	}
+	if req.URL.Host != "www.x.org" {
+		t.Fatal("unexpected host")
+	}
+	data, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !bytes.Equal(data, []byte("antani")) {
+		t.Fatal("unexpected body")
 	}
 	root := RequestMeasurementRoot(req)
 	if root == nil {
